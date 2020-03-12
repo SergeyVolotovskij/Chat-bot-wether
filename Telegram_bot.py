093ae0caf35b82207ac5f8f517758d3d0a6c246a
SergeyVolotovskij <61363925+SergeyVolotovskij@users.noreply.github.com>
2020-03-13 00:59:30 +0300

    Добавил возможность вывода других метрик погоды

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -10,6 +10,15 @@ w = observation.get_weather()
 #Вытягиваем температуру и скорость ветра
 temp = w.get_temperature('celsius') ["temp"]
 speed = w.get_wind() ["speed"]
+tempmin = w.get_temperature('celsius') ["temp_min"]
+tempmax = w.get_temperature('celsius') ["temp_max"]
+
+#ВЫВОДИМ РЕЗУЛЬТАТ
+print("В ГОРОДЕ СЕЙЧАС: " + w.get_detailed_status())
+print("МИНИМАЛЬНАЯ ТЕМПЕРАТУРА СОСТАВИТ: " + str(tempmin) + " C0")
+print("ТЕКУЩАЯ ТЕМПЕРАТУРА СОСТАВЛЯЕТ: " + str(temp)+ " C0")
+print("МАКСИМАЛЬНАЯ ТЕМПЕРАТУРА СОСТАВИТ: " + str(tempmax)+ " C0")
+print("СКОРОСТЬ ВЕТРА СОСТАВЛЯЕТ: " + str(speed) + " М/С")
 
 #заходим в телегу и получаем токен нашего телебота
 # определяем переменную с токеном телебота
@@ -19,7 +28,7 @@ bot = telebot.TeleBot('932525918:AAGDj4LRNCBi0hFo59vclpgYdavF4_vuxEU')
 
 @bot.message_handler(content_types=['text'])
 def send_welcome(message):
-	bot.send_message(message.chat.id,"Доброго времени суток Сергей Николаевич!" + " \nТекущая температура в Минске составляет: " + str(temp)+ " C0." + " \nСкорость ветра составляет: " + str(speed) + " м/с")
+	bot.send_message(message.chat.id,"Доброго времени суток Сергей Николаевич!" +"\nВ городе сейчас: " + w.get_detailed_status() + "\nТекущая температура в Минске составляет: " + str(temp)+ " C0." + " \nСкорость ветра составляет: " + str(speed) + " м/с" + )
 bot.polling(none_stop = True)
 
 
